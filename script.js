56a677af4405933e617624361a7809bbaaf3dff3
yole <igbavagodswill4@gmail.com>
2023-03-16 00:45:21 +0000

    made script follow the DRY principle

--- script.js
+++ script.js
@@ -16,45 +16,19 @@ const btnArr = [btn1, btn2, btn3, btn4, btn5]
 //default setting for thankYouState to display none
 thankYouState.style.display = 'none';
 
-//button click event
-// ;
-
+//button click function
 btnArr.forEach(function(el, i) {
+    const btnClick = el.addEventListener('click', function() {
+        el.style.background = 'hsl(25, 97%, 53%)';
+        el.style.color = 'white';
+        rating = i + 1;
+        ratingText.innerHTML = `You selected ${rating} out of 5`;
+    });
+
     if (i === 0) {
-        btn1.addEventListener('click', function() {
-                btn1.style.background = 'hsl(25, 97%, 53%)';
-                btn1.style.color = 'white';
-                rating = i + 1;
-                ratingText.innerHTML = `You selected ${rating} out of 5`;
-          })
-    } else if(i === 1) {
-        btn2.addEventListener('click', function() {
-            btn2.style.background = 'hsl(25, 97%, 53%)';
-            btn2.style.color = 'white';
-            rating = i + 1;
-            ratingText.innerHTML = `You selected ${rating} out of 5`;
-      })
-    } else if(i === 2) {
-        btn3.addEventListener('click', function() {
-            btn3.style.background = 'hsl(25, 97%, 53%)';
-            btn3.style.color = 'white';
-            rating = i + 1;
-            ratingText.innerHTML = `You selected ${rating} out of 5`;
-      })
-    } else if(i === 3) {
-        btn4.addEventListener('click', function() {
-            btn4.style.background = 'hsl(25, 97%, 53%)';
-            btn4.style.color = 'white';
-            rating = i + 1;
-            ratingText.innerHTML = `You selected ${rating} out of 5`;
-      })
-    } else if(i === 4) {
-        btn5.addEventListener('click', function() {
-            btn5.style.background = 'hsl(25, 97%, 53%)';
-            btn5.style.color = 'white';
-            rating = i + 1;
-            ratingText.innerHTML = `You selected ${rating} out of 5`;
-      })
+        btnClick
+    } else {
+        btnClick
     }
 });
 
